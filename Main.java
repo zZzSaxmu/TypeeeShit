68004ec061c702b9c04e370f92bc333d82f32818
Samu <sami.alzate.correa@gmail.com>
2024-11-12 14:44:48 -0500

    FolderPokemonType

--- Main.java
+++ Main.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
-package com.mycompany.a;
-
-/**
- *
- * @author Dylan
- */
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hello World!");
